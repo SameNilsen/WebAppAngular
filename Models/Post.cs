daeaee2ff8e9084e2db021e44bb56e89ca7332f9
SameNilsen <mort1.nilsen@gmail.com>
2023-11-23 13:24:01 +0100

    commit to be reverted

--- Models/Post.cs
+++ Models/Post.cs
@@ -8,6 +8,7 @@ namespace OsloMetAngular.Models
 {
     // Data model (data schema) for DB
     // User, Comments and UserVotes are navigation properties.
+    //  Test for revert changes.
     public class Post
     {
         [JsonProperty("PostId")]
