45693e5f4b6b5f9af417dbacda88021b0a547cca
Kevin L. A <s364530@oslomet.no>
2023-11-27 12:56:42 +0100

    comments for Models part 2

--- WebAppAngular/Models/ApplicationUser.cs
+++ WebAppAngular/Models/ApplicationUser.cs
@@ -4,19 +4,32 @@ using System.Text.Json.Serialization;
 
 namespace OsloMetAngular.Models;
 
+/* DATA MODEL FOR ApplicationUser */
 public class ApplicationUser : IdentityUser
 {
+    // UserID
     [JsonPropertyName("UserId")]
     public int UserId { get; set; }
+
+    // Name
     [JsonPropertyName("Name")]
     public string Name { get; set; } = string.Empty;
+
+    // List of comments
     public virtual List<Comment>? Comments { get; set; }
+
+    // IdentityUserID
     [JsonPropertyName("IdentityUserId")]
     [ForeignKey("IdentityUser")]
     public string? IdentityUserId { get; set; }
-    //public virtual IdentityUser? IdentityUser { get; set; }  //  Link to the IdentityUser.
+
+    // List of posts
     public virtual List<Post>? Posts { get; set; }
+
+    // List of user votes
     public virtual List<Upvote>? UserVotes { get; set; }
+
+    // Credebility
     [JsonPropertyName("Credebility")]
     public int Credebility { get; set; }
 }
--- WebAppAngular/Models/Comment.cs
+++ WebAppAngular/Models/Comment.cs
@@ -1,30 +1,32 @@
 ﻿using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
 
-namespace OsloMetAngular.Models
+namespace OsloMetAngular.Models;
+
+/* DATA MODEL (SCHEMA) FOR COMMENTS */
+// User and Post are navigation properties.
+public class Comment
 {
-    // Data model (data schema) for DB
-    // User and Post are navigation properties.
-    
-    public class Comment
-    {
-        [JsonProperty("CommentID")]
-        public int CommentID { get; set; }
-        [JsonProperty("CommentText")]
-        [StringLength(200)]
-        public string CommentText { get; set; } = string.Empty;
-        [JsonProperty("PostDate")]
-        public string? PostDate { get; set; }
-        [JsonProperty("UserId")]
-        public int UserId { get; set; } = 1;
-        //[Newtonsoft.Json.JsonIgnore]
-        public virtual User User { get; set; } = default!;
-        [JsonProperty("PostID")]
-        public int PostID { get; set; }
-        //[Newtonsoft.Json.JsonIgnore]
-        public virtual Post Post { get; set; } = default!;
+    // CommentID
+    [JsonProperty(nameof(CommentID))]
+    public int CommentID { get; set; }
 
+    // Description text
+    [JsonProperty(nameof(CommentText))]
+    [StringLength(200)]
+    public string CommentText { get; set; } = string.Empty;
 
-    }
+    // Post date
+    [JsonProperty(nameof(PostDate))]
+    public string? PostDate { get; set; }
+
+    // UserID
+    [JsonProperty(nameof(UserId))]
+    public int UserId { get; set; } = 1;
+    public virtual User User { get; set; } = default!;
+
+    // PostID
+    [JsonProperty(nameof(PostID))]
+    public int PostID { get; set; }
+    public virtual Post Post { get; set; } = default!;
 }
